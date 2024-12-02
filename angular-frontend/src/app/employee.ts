057ef86f7dfbf924912f076bfd4d7a956fe62f19
Kalyani <madana.kalyani02@gmail.com>
2024-12-02 23:24:31 +0530

    paste src

---
+++ angular-frontend/src/app/employee.ts
@@ -0,0 +1,17 @@
+export class Employee {
+    id: number=0;
+    firstName: string='';
+    lastName: string='';
+    emailId: string='';
+
+    constructor() {
+        this.id = 0;
+        this.firstName = '';
+        this.lastName = '';
+        this.emailId = '';
+      }
+    }
+
+
+
+
